d05215035d3336104eda803d31e157173c77fe90
Kogarashi50 <abdelhamid2003assamure@gmail.com>
2026-04-07 13:12:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Let Azure pick the port (usually 8080 or 80)
-const PORT = process.env.PORT || 8080; 
+const PORT = 8080; 
 
 // On Azure Linux, /home is the best place to write persistent files
 const FILE = path.join(__dirname, "visits.json");
